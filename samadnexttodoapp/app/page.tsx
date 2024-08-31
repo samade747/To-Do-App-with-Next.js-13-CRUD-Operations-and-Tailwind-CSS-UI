0c204fd13290ce239aaa385b776c4d9f30dfb329
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-08-31 18:41:58 +0500

    641

---
+++ samadnexttodoapp/app/page.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <main className="max-w-4xl mx-auto mt-4">
+      <div>
+
+      <h1 className="text-center underline ">Samad Next Todo App</h1>     
+            <button className="text-center my-5 flex flex-col gap-4">Click me</button>                
+            <h1 className="text-2xl font-bold"> </h1>
+      </div> 
+      
+    </main>
+  );
+}
