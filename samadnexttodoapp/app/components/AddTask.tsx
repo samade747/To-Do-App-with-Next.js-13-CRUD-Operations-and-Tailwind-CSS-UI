fb91c90343c4901a5f2664495b13dfec9915c54b
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-09-01 12:37:05 +0500

    1237

---
+++ samadnexttodoapp/app/components/AddTask.tsx
@@ -0,0 +1,13 @@
+import { AiOutlinePlus } from "react-icons/ai"
+
+
+const AddTask = () => {
+  return (
+    <div className="btn btn-primary w-full">
+    <AiOutlinePlus className="ml-2" size={20} />
+
+        Add NEW TASK</div>
+  )
+}
+
+export default AddTask
--- samadnexttodoapp/app/page.tsx
+++ samadnexttodoapp/app/page.tsx
@@ -1,12 +1,16 @@
+import AddTask from "./components/AddTask";
+import TodoList from "./components/TodoList";
 
 export default function Home() {
   return (
     <main className="max-w-4xl mx-auto mt-4">
-      <div>
+      <div className="text-center my-5  flex flex-col gap-4 ">
 
-      <h1 className="text-center underline ">Samad Next Todo App</h1>     
-            <button className="text-center my-5 flex flex-col gap-4">Click me</button>                
-            <h1 className="text-2xl font-bold"> </h1>
+      <h1 className="text-2xl font-bold">TO DO APP </h1>
+
+        <AddTask />
+        <TodoList />         
+            
       </div> 
       
     </main>
